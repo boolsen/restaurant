65ec79e10d541143a1d1edd30fd7c423cea67c69
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-05-06 21:22:10 +0200

    Added json files with restaurant and menu info

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const aboutBtn = document.querySelector(".about");
+
+homeBtn.addEventListener('click', () => {
+    swapView('home');
+});
+
+function swapView(view){
+    console.log('test');
+    
+}
